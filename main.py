e14ab2fa5b6860e306d309f8e0799c1d2eb11b4e
David Domínguez <dominguezbarradavid33@gmail.com>
2019-02-08 12:23:36 +0100

    Added main file. Now you can test the program

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# No std libs imported
+import random
 
 from penv import Penv
 from rl_dir import RL
@@ -10,6 +10,23 @@ q = Persistance()
 q.flush()
 
 rl = RL(s_dim = 1, a_dim = num_arms, Q = q)
+rl.DEBUG = True
+q.DEBUG = True
+env.DEBUG = True
+s = 0
+alpha = 0.01
+actions = list(range(num_arms))
 
-rl.update(s, a, v)
+step = 0
+while step < 10000:
+    exploration = random.random()
+    if exploration < alpha:
+        a = random.choice(actions)
+    else:
+        a = rl.get_best_action(s)
+    r = env.step(a)
+    rl.update(s, a, r)
+    step += 1
 
+best = max(enumerate(env.info()), key = lambda x: x[1].mean)
+print(best)
